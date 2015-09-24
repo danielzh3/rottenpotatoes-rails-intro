2029c011eb7431ea0fdeb0c9c98cdaee4d44d9bd
danielzh3 <danielgoabroad@gmail.com>
2015-09-23 23:10:24 -0500

    complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,53 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.fetch_ratings
+
+    # case when all setting are unchecked
+    if (( !params[:select] and !params[:ratings] and !params[:orderby]) and (session[:select] or session[:ratings] or session[:orderby]))
+      if ( !params[:select] and session[:select] )
+        params[:select] = session[:select]
+      end
+      if ( !params[:orderby] and session[:orderby] )
+        params[:orderby] = session[:orderby]
+      end
+
+      redirect_to movies_path(:select => params[:select], :orderby => params[:orderby], :ratings => params[:ratings]) 
+
+    else
+
+      if (params[:select])
+        @picked = params[:select].scan(/[\w-]+/)
+        session[:select] = params[:select]
+      else
+        @picked = params[:ratings] ? params[:ratings].keys : []
+        session[:select] = params[:ratings] ? params[:ratings].keys.to_s : nil
+      end
+      
+      session[:orderby] = params[:orderby]
+      session[:ratings] = params[:ratings]
+
+      if (params[:orderby] == "title")  #sort by title
+          if (params[:ratings] or params[:select])
+            @movies = Movie.where("rating" => (@picked==[] ? @all_ratings : @picked)).order("title")
+          else
+            @movies = Movie.order("title")
+          end
+      elsif (params[:orderby] == "release_date")  # sort by date
+          if (params[:ratings] or params[:select]) 
+            @movies = Movie.where("rating" => (@picked==[] ? @all_ratings : @picked)).order("release_date")
+          else
+            @movies = Movie.order("release_date")
+          end
+      elsif (params[:orderby] == nil) 
+          if (params[:ratings] or params[:select]) 
+            @movies = Movie.where("rating" => @picked)
+          else
+            @movies = Movie.all
+          end
+      end
+
+    end
   end
 
   def new
@@ -19,7 +65,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -30,7 +76,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+  def self.fetch_ratings
+    ratings = []
+    Movie.all.each do |movie|
+	      if (!ratings.find_index(movie.rating))
+	        	ratings.push(movie.rating)
+	      end
+    end
+    return ratings
+  end
 end
